87f02e8ead6326a9e532db1290cfddfdea22653e
dr6g1 <pd@pd-pc.localdomain>
2021-08-28 21:19:02 +0200

    1

--- initscript-0x2e8b.sh
+++ initscript-0x2e8b.sh
@@ -6,10 +6,22 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]
     then
         exit 1
 	fi
-cd $dir || exit 
-if test -f "$dir"/dotfiles; then
-    echo "katalog już istnieje"
+cd "$dir" || exit 
+if [ -d "$dir"/dotfiles ]; then
+    read -p "katalog już istnieje, czy chcesz usunąć i pobrać od nowa?" -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^^[Yy]$ ]]
+	then 
+	exit 1
+	fi
 else
+    echo "usuwam dotfiles"
+    rm -rf "$dir"/dotfiles
+    rm "$dir"/.vimrc
+    rm "$dir"/.tmux.conf
+    echo "ściagam z gita"
     git clone https://github.com/dr6g1/dotfiles
+    find "$dir"/dotfiles -maxdepth 1 -type f \( -name ".*" ! -name ".git" \) | while read file;do ln -s "$file" "$dir"/;done
 fi
-find "$dir"/dotfiles -maxdepth 1 -type f \( -name ".*" ! -name ".git" \) | while read file;do ln -s "$file" "$dir"/;done
+echo "Skrypt gotowy"
+
