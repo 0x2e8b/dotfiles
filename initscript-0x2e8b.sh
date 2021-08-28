206b0c930ba8cdb1e22802d1d3262c4bef37b272
dr6g1 <pd@pd-pc.localdomain>
2021-08-28 21:31:03 +0200

    update

--- initscript-0x2e8b.sh
+++ initscript-0x2e8b.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dir=/home/"$(whoami)"
-read -p  "będziem robić init, gotowy? [y/N]" -n 1 -r
+read -p  "Będziem robić init, gotowy? [y/N]" -n 1 -r
 echo    
 if [[ ! $REPLY =~ ^[Yy]$ ]]
     then
@@ -8,20 +8,20 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]
 	fi
 cd "$dir" || exit 
 if [ -d "$dir"/dotfiles ]; then
-    read -p "katalog już istnieje, czy chcesz usunąć i pobrać od nowa?" -n 1 -r
+    read -p "Katalog już istnieje, czy chcesz usunąć i pobrać od nowa? [y/N]" -n 1 -r
     echo
     if [[ ! $REPLY =~ ^^[Yy]$ ]]
 	then 
+	echo "Nic nie robię"
 	exit 1
-	fi
-else
-    echo "usuwam dotfiles"
-    rm -rf "$dir"/dotfiles
-    rm "$dir"/.vimrc
-    rm "$dir"/.tmux.conf
-    echo "ściagam z gita"
-    git clone https://github.com/dr6g1/dotfiles
-    find "$dir"/dotfiles -maxdepth 1 -type f \( -name ".*" ! -name ".git" \) | while read file;do ln -s "$file" "$dir"/;done
-fi
+    else
+	echo "Usuwam dotfiles"
+	rm -rf "$dir"/dotfiles
+	rm "$dir"/.vimrc
+        rm "$dir"/.tmux.conf
+        echo "Ściągam z gita"
+        git clone https://github.com/dr6g1/dotfiles
+        find "$dir"/dotfiles -maxdepth 1 -type f \( -name ".*" ! -name ".git" \) | while read file;do ln -s "$file" "$dir"/;done
+    fi
 echo "Skrypt gotowy"
-
+fi
